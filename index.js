658c77dd2776f0dc8c461251a82263f6479e7225
Fabricio Tuosto <fabricio.tuosto@gmail.com>
2016-03-02 17:57:46 -0300

    Add terminal markdown render

--- index.js
+++ index.js
@@ -4,20 +4,36 @@ var server = 'localhost:5000'
 , ws = new WebSocket('ws://'+server)
 , colors = require('colors')
 , readline = require('readline')
+, marked = require('marked')
+, TerminalRenderer = require('marked-terminal')
 , rl = readline.createInterface({
   input: process.stdin,
   output: null,
   terminal: false
 });
 
+function getMessage(str){
+  try {
+    var json = JSON.parse(str);
+    return json.attachments[0].title
+  } catch (e) {
+    return str;
+  }
+}
+
+marked.setOptions({ renderer: new TerminalRenderer() });
+
 ws.on('open', function(){ console.log("\nHoyPido: ".bold, 'Hola capo', '\n'); });
 ws.on('close', function(){ console.log("\nHoyPido: ".bold, 'Se cayó la conexión :P intenta nuevamente', '\n'); process.exit(); });
-ws.on('message', function(data, flags){ console.log("\nHoyPido: ".bold, data, '\n'); });
+ws.on('message', function(data, flags){
+  var message = getMessage(data);
+  console.log("\nHoyPido: ".bold, marked(message), '\n');
+});
 
 rl.on('line', function(line) {
   if(!line){ return; };
   ws.send(JSON.stringify({
-    "text":line, 
+    "text":line,
     "user":{
       "id":"pepe",
       "profile":{
