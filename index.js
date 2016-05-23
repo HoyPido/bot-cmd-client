89f199c6180909ec4dc9c043228993338c5cd715
Fabricio Tuosto <fabricio.tuosto@gmail.com>
2016-05-23 11:50:15 -0300

    Fix merge

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 #!/usr/bin/env node
 
-<<<<<<< HEAD
-//var server = 'localhost:5001'
-=======
-// var server = 'localhost:5001'
->>>>>>> e3c6c63f5722aadd0afadb4a63ef8d4902c7906b
 var server = 'hoypido-slackbot.herokuapp.com'
 
 , WebSocket = require('ws')
